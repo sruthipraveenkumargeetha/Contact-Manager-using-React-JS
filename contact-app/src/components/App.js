80e65959c883a26e2cf467b2dea84c03dd798635
SruthiPraveenKGeetha <spg003@ucsd.edu>
2022-11-02 01:37:03 -0700

    added Header component

--- contact-app/src/components/App.js
+++ contact-app/src/components/App.js
@@ -1,8 +1,16 @@
+import React from 'react';
+import Header from "./Header";
+//import AddContact from "./AddContact";
+//import ContactList from "./ContactList";
 import './App.css';
 
 function App() {
   return (
-    <div> Hello World</div>
+    <div> 
+      <Header />
+      {/*<AddContact />
+      <ContactList /> */}
+    </div>
   );
 }
 
