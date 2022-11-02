bb7ff0838a239b925d50d6e90bb40da4466fadca
SruthiPraveenKGeetha <spg003@ucsd.edu>
2022-11-02 02:13:16 -0700

    added renderContactList function to render an array of contacts with delete icon

--- contact-app/src/components/App.js
+++ contact-app/src/components/App.js
@@ -5,11 +5,25 @@ import ContactList from "./ContactList";
 import './App.css';
 
 function App() {
+
+  const contacts = [
+    {id: "1", 
+    name: "Aishwarya", 
+    email: "rputchak@ucsd.edu"},
+
+    {id: "2", 
+    name: "Chitra", 
+    email: "chitra@ucsd.edu"},
+
+    {id: "3",
+    name: "Sruthi",
+    email: "spg003@ucsd.edu"},
+  ];
   return (
     <div className='ui container'> 
       <Header />
       <AddContact />
-      <ContactList /> 
+      <ContactList contacts = {contacts} /> 
     </div>
   );
 }
