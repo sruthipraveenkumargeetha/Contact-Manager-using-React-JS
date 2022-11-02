3d9e9d13fcc76d9d5d2a45c19df8410835b75f13
SruthiPraveenKGeetha <spg003@ucsd.edu>
2022-11-02 02:29:50 -0700

    refactored code into ContactCard and added avatar

---
+++ contact-app/src/components/ContactCard.js
@@ -0,0 +1,28 @@
+import React from "react";
+import user from "../images/user.jpg"
+const ContactCard = (props) => {
+    const {_, name, email} = props.contact
+    return (
+
+        <div className="item">
+            <img className="ui avatar image" src={user} alt="user" />
+        <div className="content">
+            <div className="header">
+                {name}
+            </div>
+            <div>
+                {email}
+            </div>
+        </div>
+        <i 
+        className="trash alternate outline icon"
+        style = {{ color: "red", marginTop: "7px"}}> </i>
+    </div>
+
+
+
+    );
+};
+
+
+export default ContactCard;
