31b4c616260e376f7db30099246345186828eb02
SruthiPraveenKGeetha <spg003@ucsd.edu>
2022-11-03 02:40:07 -0700

    adding persistent storage

--- contact-app/src/components/App.js
+++ contact-app/src/components/App.js
@@ -1,14 +1,25 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from "./Header";
 import AddContact from "./AddContact";
 import ContactList from "./ContactList";
 import './App.css';
 
 function App() {
+  const LOCAL_STORAGE_KEY = "contacts"
   const addContactHandler = (contact) =>{
     setContacts([...contacts, contact]);
   }
-  const [contacts, setContacts] = useState([]);
+  const [contacts, setContacts] = useState(() => {
+    // getting stored value
+    const retrievedContacts = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    return retrievedContacts || [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts));
+  }, [contacts]);
+
+
   return (
     <div className="ui container"> 
       <Header />
